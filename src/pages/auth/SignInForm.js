060fb1c7dac4e2bb3a892164f9026db8e1072c08
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-11-17 19:02:59 +0000

    _

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -14,7 +14,7 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import { useSetCurrentUser } from "../../context/CurrentUserContext";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
