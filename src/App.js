80b30fddd2a2208e0cdc03f4a195a73288338ced
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-11-18 00:15:13 +0000

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
-import {Route,Switch} from 'react-router-dom'
-import "./api/axiosDefaults"
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
-import PostCreateForm from './pages/posts/PostCreateForm';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 function App() {
-
   return (
     <div className={styles.App}>
       <NavBar />
@@ -17,7 +17,8 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/post/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
